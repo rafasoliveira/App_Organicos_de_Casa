23a202d53f4af1923830e1fbb92b1a5641b7bc6a
rafasoliveira <103112389+rafasoliveira@users.noreply.github.com>
2022-12-12 19:40:49 -0300

    12/12/2022 - Atualização e  criação das atividades

--- app/src/main/java/com/odc/organicosdecasa/Activity/FormCadastroActivity.java
+++ app/src/main/java/com/odc/organicosdecasa/Activity/FormCadastroActivity.java
@@ -1,12 +1,16 @@
 package com.odc.organicosdecasa.Activity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.odc.organicosdecasa.R;
 
 public class FormCadastroActivity extends AppCompatActivity {
+
+    EditText editTextEmail, editTextSenha, editTextNome, editTextSobrenome, editTextDtAniversario, editTextCelular, editTextCEP,editTextEndereco, editTextNumEndereco,
+            editTextComplemento, editTextCidade, editTextUF, editTextBairro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
